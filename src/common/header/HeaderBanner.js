ea685867e5f1dd4bf87d4de2c79b1c193a021a85
Tom Irimescu <tom@irimescu.com>
2020-07-01 09:58:01 -0400

    Add header banner, style thank you page

---
+++ src/common/header/HeaderBanner.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+export const HeaderBanner = () => {
+  return (
+    <div className="relative flex-shrink-0 flex h-16 bg-white shadow">
+      <div className="flex-1 px-4 flex justify-between bg-blue">
+        <div className="flex-1 flex">
+          <div className="w-full flex md:ml-0">
+            <div className="relative w-full text-redDark focus-within:text-redDark">
+              <div className="absolute inset-y-0 left-0 flex items-center pointer-events-none">
+                <h1 className="text-sm md:text-xl lg:text-2xl font-semibold text-red whitespace-no-wrap">
+                  IRI-STORE
+              </h1>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/store/Thanks.js
+++ src/store/Thanks.js
@@ -1,22 +1,25 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import { HeaderBanner } from './../common/header/HeaderBanner';
 
 export class Thanks extends Component {
 
-    render() {
-        return <div>
-            <div className="col bg-dark text-white">
-                <div className="navbar-brand">SPORTS STORE</div>
-            </div>
-            <div className="m-2 text-center">
-                <h2>Thanks!</h2>
-                <p>Thanks for placing your order.</p>
-                <p>Your order is #{ this.props.order ? this.props.order.id : 0 }</p>
-                <p>We'll ship your goods as soon as possible.</p>
-                <Link to="/store" className="btn btn-primary">
-                    Return to Store
-                </Link>
-            </div>
+  render() {
+    return (
+      <>
+      <HeaderBanner />
+        <div>
+          <div className="m-2 text-center mt-8 mx-auto">
+            <h2 className="text-lg font-bold mb-6">Thanks!</h2>
+            <p className='mb-3'>Thank you for your business.</p>
+            <p className="break-words w-64 mx-auto">Your order number: <span className="font-semibold">{this.props.order ? this.props.order.id : 0}</span></p>
+            <p>is on the way...</p>
+              <Link to="/store" className="sideBarButton w-40 mt-8">
+                Return to Store
+              </Link>
+          </div>
         </div>
-    }
+      </>
+    ) 
+  }
 }
