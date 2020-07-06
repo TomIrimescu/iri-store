1f6dfd94f0e4a87389bb63bdf69c98f163b93495
Tom Irimescu <tom@irimescu.com>
2020-07-06 17:50:01 -0400

    Detach node code to server app for prod

--- src/data/Urls.js
+++ src/data/Urls.js
@@ -12,3 +12,12 @@ export const RestUrls = {
 
 export const GraphQlUrl = `${protocol}://${hostname}:${port}/graphql`;
 export const authUrl = `${protocol}://${hostname}:${port}/login`;
+
+// export const RestUrls = {
+//   [DataTypes.PRODUCTS]: `https://us-central1-irimescu-9d5e8.cloudfunctions.net/app/api/products`,
+//   [DataTypes.CATEGORIES]: `https://us-central1-irimescu-9d5e8.cloudfunctions.net/app/api/categories`,
+//   [DataTypes.ORDERS]: `https://us-central1-irimescu-9d5e8.cloudfunctions.net/app/api/orders`
+// }
+
+// export const GraphQlUrl = `https://us-central1-irimescu-9d5e8.cloudfunctions.net/app/graphql`;
+// export const authUrl = `https://us-central1-irimescu-9d5e8.cloudfunctions.net/app/login`;
